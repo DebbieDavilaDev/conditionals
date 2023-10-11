e9da304748aa962cf9750ac6df01dfe45bee31bb
Debbie Davila <debbie@debbiedavila.com>
2023-10-11 11:01:47 -0400

    If/Else message

---
+++ index.js
@@ -0,0 +1,21 @@
+let Firstname = "deb"
+
+
+// If / Else
+
+if(Firstname == "deb"){
+    console.log(`Hello ${Firstname}`)
+} else { 
+    console.log("bye, bye")
+}
+
+
+
+// create and if / else statement o see if age is greater than 21
+
+let age = "18"
+if(age > 21 ){
+    console.log(`Welcome Big ${age}`)
+} else {
+    console.log('Sorry BabyFace come back when youre 18')
+}
